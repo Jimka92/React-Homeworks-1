1d36d2bd215964fdc8e6a067b9e31f7a02595091
Avonavi <Avonavi232@gmail.com>
2018-01-26 20:27:22 +0300

    func-component: 1 done

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function wrt(d) {
+    console.log(d);
+}
+
+function Stars({count}) {
+  if(!count || count < 1 || count > 5) return;
+  const stars = [];
+  for (let i = 0; i < count; i++) {
+    stars.push(<Star/>);
+  }
+  return (
+      <ul className="card-body-stars u-clearfix">
+          {stars}
+      </ul>
+  );
 }
