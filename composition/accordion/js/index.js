41f0c3cbaae23d62a79798f003cce55839b42876
Avonavi <Avonavi232@gmail.com>
2018-02-03 20:38:56 +0300

    composition: 3 done

--- composition/accordion/js/index.js
+++ composition/accordion/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function AccordeonSection(props) {
+  return(
+    <section onClick={(e) => {e.currentTarget.classList.toggle('open')}} className={props.open? 'section open' : 'section'}>
+      <button>toggle</button>
+      <h3 className="sectionhead">{props.title}</h3>
+      <div className="articlewrap">
+        <div className="article">
+          {props.children}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Accordeon(props){
+  return(
+    <main className="main">
+      <h2 className="title">{props.title}</h2>
+      {props.children}
+    </main>
+  );
+}
+
