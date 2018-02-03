937fe439c4b261e94e216601ef259759607fedd4
Avonavi <Avonavi232@gmail.com>
2018-02-03 13:34:21 +0300

    composition: 1 done

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,45 @@
 'use strict';
 
+/*много вариантов решения. например
+* 0. Создать обертку для Item и библиотеку цветов. Вероятно, они всегда одинаковыми будут, но в случае изменения логики выбора цвета - будем менять компонент ColoredItem
+* 1. создать компонент Catalog
+* 2. создать функцию getCatalogContent. Практически то же самое. что лучше?
+*/
+
+const colorLib = {
+  'unisex': 'black',
+  'male': 'blue',
+  'female': 'orange'
+};
+
+const ColoredItem = ({item}) => {
+  return(
+    <Item color={colorLib[item.type]} item={item}/>
+  )
+};
+
+const Catalog = ({items}) => {
+  return (
+    <main>
+      {items.map(item => {
+        return <ColoredItem item={item}/>
+      })}
+    </main>
+  )
+};
+
+//пример функции
+// const getCatalogContent = (items) => {
+//   return items.map(item => {
+//     return <ColoredItem item={item}/>
+//   });
+// };
+
 const App = ({items}) => (
-  <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
-  </main>
+  <Catalog items={items}/>
+
+  //пример использования 2го варианта
+  // <main>
+  // {getCatalogContent(items)}
+  // </main>
 );
