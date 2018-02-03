bb994edad0d430c7fd3f6c31a714ffb6a1ae74bd
Avonavi <Avonavi232@gmail.com>
2018-02-03 08:58:45 +0300

    state: 1 done

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,20 +1,5 @@
 'use strict';
 
-// const App = props => (
-//   <div>
-//     <Toolbar
-//       filters={props.filters}
-//       selected={'All'}
-//       onSelectFilter={(filter) => console.log(filter)} />
-//     <Portfolio projects={props.projects} />
-//   </div>
-// );
-
-function wrt(d) {
-  console.log(d);
-}
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
