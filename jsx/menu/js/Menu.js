b639bed37cda9fc4d93faee066be4a5c904970b0
Avonavi232 <Avonavi232@gmail.com>
2018-02-04 12:17:58 +0300

    JSX: 2 правки внесены

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,28 +1,16 @@
 const Menu = function ({items, opened}) {
+    const listItems = items.map( (item, index) => <li key={index}><a href={item.href}>{item.title}</a></li> );
 
-    const listItems = items.map( (item, index) => {
-        return (
-            <li key={index}><a href={"#" + item.href}>{item.title}</a></li>
-        )
-    } );
-
-    if(opened){
-        return(
-            <div className="menu menu-open">
-                <div className="menu-toggle"><span></span></div>
+    return(
+        <div className={"menu " + (opened && "menu-open")}>
+            <div className="menu-toggle"><span></span></div>
+            {opened && (
                 <nav>
                     <ul>
                         {listItems}
                     </ul>
                 </nav>
-            </div>
-        )
-    } else {
-        return(
-            <div className="menu">
-                <div className="menu-toggle"><span></span></div>
-            </div>
-        )
-    }
-
+            )}
+        </div>
+    );
 };
