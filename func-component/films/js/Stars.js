7e1820d979abd58d90707c87c445ff1f38a74ceb
Avonavi <Avonavi232@gmail.com>
2018-02-07 23:27:10 +0300

    func-component 2 доработано

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,9 +1,11 @@
 'use strict';
 function Stars({count}) {
-  if(!count || count < 1 || count > 5) return;
+  if(!count || count < 1 || count > 5) return null;
   const stars = [];
   for (let i = 0; i < count; i++) {
-    stars.push(<Star/>);
+    stars.push(
+      <li key={i}><Star/></li>
+    );
   }
   return (
       <ul className="card-body-stars u-clearfix">
