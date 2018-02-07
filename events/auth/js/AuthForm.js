510e8e21439277ca60db5aa02a6e7958339f3d8d
Avonavi <Avonavi232@gmail.com>
2018-02-07 23:57:56 +0300

    events 2 доработано

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,60 +1,67 @@
 'use strict';
 
 function AuthForm(props) {
-    let name, email, password;
-    return(
-        <form onSubmit={formSubmit} className="ModalForm" action="/404/auth/" method="POST">
-            <div className="Input">
-                <input required
-                       onChange={inputValidate}
-                       type="text"
-                       placeholder="Имя"
-                       ref={element => name = element}
-                />
-                    <label></label>
-            </div>
-            <div className="Input">
-                <input
-                    type="email"
-                    onChange={inputValidate}
-                    placeholder="Электронная почта"
-                    ref={element => email = element}
-                />
-                    <label></label>
-            </div>
-            <div className="Input">
-                <input
-                    required
-                    onChange={inputValidate}
-                    type="password"
-                    placeholder="Пароль"
-                    ref={element => password = element}
-                />
-                    <label></label>
-            </div>
-            <button type="submit">
-                <span>Войти</span>
-                <i className="fa fa-fw fa-chevron-right"></i>
-            </button>
-        </form>
-    );
+  let form;
+  return (
+    <form
+      onSubmit={formSubmit}
+      className="ModalForm"
+      action="/404/auth/"
+      method="POST"
+      ref={element => form = element} >
+      <div className="Input">
+        <input required
+               onChange={inputValidate}
+               type="text"
+               placeholder="Имя" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input
+          type="email"
+          onChange={inputValidate}
+          placeholder="Электронная почта" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input
+          required
+          onChange={inputValidate}
+          type="password"
+          placeholder="Пароль" />
+        <label></label>
+      </div>
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>
+    </form>
+  );
 
-    function formSubmit(e) {
-        e.preventDefault();
-        props.onAuth({
-            name: name.value,
-            email: email.value,
-            password: password.value,
-        });
-    }
+  function formSubmit(e) {
+    e.preventDefault();
 
-    function inputValidate(e) {
-        const input = e.nativeEvent.target;
-        if (input.getAttribute('type') === 'email'){
-            input.value = input.value.replace(/[^a-zA-Z0-9@.\-_]/, '');
-        } else if (input.getAttribute('type') === 'password'){
-            input.value = input.value.replace(/[^a-zA-Z0-9_]/, '');
-        }
+    const name = form.querySelector('input[type="text"]').value;
+    const email = form.querySelector('input[type="email"]').value;
+    const password = form.querySelector('input[type="password"]').value;
+
+    if (!name || !email || !password)
+      return null;
+
+    props.onAuth({
+      name: name,
+      email: email,
+      password: password
+    });
+  }
+
+  function inputValidate(e) {
+    const input = e.nativeEvent.target;
+    if (input.getAttribute('type') === 'email') {
+      input.value = input.value.replace(/[^a-zA-Z0-9@.\-_]/, '');
+    } else if (input.getAttribute('type') === 'password') {
+      input.value = input.value.replace(/[^a-zA-Z0-9_]/, '');
     }
+  }
 }
 
