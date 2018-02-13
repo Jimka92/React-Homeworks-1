da545b82d4f1e189083430a2941011ca9273700d
Aleksandr Ivanov <avonavi232@gmail.com>
2018-02-13 17:49:49 +0300

    ctrl-component: 3 done

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,33 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
+const FontSelector = ({fonts, selected, onSelect}) => {
     return (
         <div className="font-picker">
-            Выберите шрифт
+            {
+                fonts.map(function (font, index) {
+                    return(
+                        <div className="grid center font-item">
+                            <input
+                                type="radio"
+                                name="font"
+                                value={"abc" + (index + 1)}
+                                id={"abc" + (index + 1)}
+                                onChange={function () {
+                                    onSelect(font);
+                                }}
+                                checked={font === selected}
+                            />
+                            <label
+                                htmlFor={"abc" + (index + 1)}
+                                className={"grid-" + (index + 1)}
+                            >
+                                <PictureFont
+                                    text='abc'
+                                    path={font.path}
+                                />
+                            </label>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 };
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,18 @@
 const TextRenderLine = ({value, onChange}) => {
 	return (
 		<div className="type-text">
-			Наберите текст
+			<textarea
+				name="text"
+				id="font-text"
+				cols="30"
+				rows="2"
+				placeholder="Введите текст для футболки"
+				value={value}
+				onChange={(e) => {
+					onChange(e.currentTarget.value);
+				}}
+			>
+			</textarea>
 		</div>
 	);
 };
