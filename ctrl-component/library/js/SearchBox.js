a10951f24afb1e9180acbdfc82193b3a651747a9
Aleksandr Ivanov <avonavi232@gmail.com>
2018-02-13 17:06:02 +0300

    ctrl-component: 2 done

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,15 @@
-const SearchBox = () => {
+const SearchBox = ({value, filterBooks}) => {
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+        <input
+            type="text"
+            placeholder="Поиск по названию или автору"
+            value={value}
+            onChange={(e) => {
+                console.log(e.currentTarget.value);
+                if (typeof filterBooks !== 'function')
+                    return;
+                filterBooks(e.currentTarget.value);
+            }}
+        />
     );
 };
